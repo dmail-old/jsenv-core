ee31916ce545198d1824eb556f2afb06febdcff8
Damien Maillard <dmaillard06@gmail.com>
2015-09-20 21:51:14 +0200

    fix cwd on platform run

--- run.js
+++ run.js
@@ -1,10 +1,17 @@
-require('./index.js');
+function run(location){
+	var path = require('path');
+	var cwd = process.cwd();
+	var fsFileLocation = path.resolve(cwd, location);
+	var dirname = path.dirname(fsFileLocation);
 
-function run(path){
+	process.chdir(dirname);
+	location = path.basename(location);
+
+	require(path.resolve(__dirname, './index.js'));
+
 	global.platform.ready(function(){
-		global.platform.info('running', global.platform.locate(path));
-
-		return System.import(path);
+		global.platform.info('running', global.platform.locate(location));
+		return System.import(location);
 	});
 }
 
