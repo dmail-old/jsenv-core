c568606a703b714f8ed0ced7f1384a059490fa49
Damien Maillard <dmaillard06@gmail.com>
2016-06-04 11:53:03 +0200

    sourcemap ok on error stack

--- test/sourcemap-error-stack-test.js
+++ test/sourcemap-error-stack-test.js
@@ -2,22 +2,29 @@
 
 import jsenv from 'jsenv';
 
+import assert from '@node/assert';
+
 var source = `
 export default function() {
     throw new Error("here");
 }
 `;
 var sourceAddress = 'anonymous';
+// var selfUrl = env.mainAction.module.href;
 
 Promise.resolve().then(function() {
     return jsenv.generate({logLevel: 'info'}).then(function(env) {
-        // jsenv.exceptionHandler.add(function(error) {
-        //     console.log('handling exception', error);
-        //     return true;
-        // });
-
         return env.evalMain(source, sourceAddress).then(function(exports) {
             exports.default();
+        }).catch(function(error) {
+            assert.equal(error.message, 'here');
+            return error.prepare().then(function() {
+                return error;
+            });
+        }).then(function(error) {
+            assert.equal(error.fileName, env.locate('anonymous'));
+            assert.equal(error.lineNumber, 3);
+            console.log('error.fileName & error.lineNumber ok with transpiled anonymous module');
         });
     });
 });
