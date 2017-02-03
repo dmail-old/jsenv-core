8fab764bef5806e80af2ccf7a0ceea6b66beffe3
Damien Maillard <dmaillard06@gmail.com>
2017-02-03 14:15:28 +0100

    adding explicit eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    "extends": "./eslint.js",
+    "rules": {
+        /*
+        because it seems like a good idea at first (to force specific quote style) but then
+        you fall into edge case where you want to keep quote or not for good reasons
+        and you dont want a too restrictive rule to get in your way
+        */
+        "quote-props": [
+            "error",
+            "as-needed",
+            {
+                "keywords": true,
+                "numbers": true,
+                "unnecessary": false
+            }
+        ]
+    }
+};
