90198f31a3d5f3c50eaf4755bae92342eb391203
Damien Maillard <dmaillard06@gmail.com>
2017-02-14 12:37:51 +0100

    improve feature api

--- src/features/api.js
+++ src/features/api.js
@@ -20,141 +20,6 @@ var featureTranspiler = require('./transpiler.js')({
     ]
 });
 
-// maintenant, ça pourrait être plutôt cool d'obtenir une sorte de js qu'on a plus qu'à évaluer
-// pour obtenir une liste des features
-// var features = eval(createFeatures())
-// ensuite on aurait plus qu'à check si on a déjà un résultat pour chaque feature
-// toutes celles n'ayant pas de résultat doivent être testées
-// et on doit alors crée encore un autre code qu'on doit eval
-// genre createFeatureTests(...names)
-// ou tout simplement on commence par createFeatureTests
-// si aucun tets a run alors createFeaturesPolyfill()
-// et en parallèle createFeaturesFixedTests()
-// si aucun createFeaturesFixedTests necéssaire on retourne que c'est bon
-// sinon on retourne les tests qu'on doit faire
-
-var featuresStore = store.memoryEntry();
-var getFeaturesSource = (function() {
-    function createFeaturesSource() {
-        var featureEntries = [];
-
-        function readFileSystemFolder(featureName) {
-            var path = featuresFolderPath;
-            if (featureName) {
-                path += '/' + featureName;
-            }
-
-            var featureEntry = {
-                name: featureName,
-                code: ''
-            };
-            if (featureName) {
-                featureEntries.push(featureEntry);
-            }
-
-            return fsAsync('readdir', path).then(function(ressourceNames) {
-                var ressourcePaths = ressourceNames.map(function(name) {
-                    return path + '/' + name;
-                });
-                var ressourcesPromise = ressourcePaths.map(function(ressourcePath, index) {
-                    return fsAsync('stat', ressourcePath).then(function(stat) {
-                        var ressourceName = ressourceNames[index];
-                        if (stat.isFile()) {
-                            if (ressourceName === 'feature.js') {
-                                return featureTranspiler.transpileFile(ressourcePath, {
-                                    sourceMaps: false,
-                                    filename: false,
-                                    transform: function(code) {
-                                        return code;
-                                    }
-                                }).then(function(code) {
-                                    featureEntry.code = code;
-                                });
-                            }
-                        } else if (stat.isDirectory()) {
-                            if (ressourceName[0].match(/[a-z]/)) {
-                                var dependentFeatureName;
-                                if (featureName) {
-                                    dependentFeatureName = featureName + '/' + ressourceName;
-                                } else {
-                                    dependentFeatureName = ressourceName;
-                                }
-
-                                return readFileSystemFolder(dependentFeatureName);
-                            }
-                        }
-                        return Promise.resolve();
-                    });
-                });
-                return Promise.all(ressourcesPromise);
-            });
-        }
-
-        function convertEntries() {
-            var registerFeaturesHead = 'jsenv.registerFeatures(function(registerFeature, transpile) {';
-            var registerFeaturesBody = featureEntries.map(function(featureEntry) {
-                var featureNameSource = "'" + featureEntry.name + "'";
-                var featurePropertiesSource = '';
-                if (featureEntry.code) {
-                    featurePropertiesSource += 'function(feature) {\n\t';
-                    featurePropertiesSource += featureEntry.code;
-                    featurePropertiesSource += '\n}';
-                } else {
-                    featurePropertiesSource = 'function() {}';
-                }
-
-                return 'registerFeature(' + featureNameSource + ', ' + featurePropertiesSource + ');';
-            });
-            var registerFeaturesFoot = '});';
-
-            return (
-                '\n' +
-                registerFeaturesHead +
-                '\n\t' +
-                registerFeaturesBody.join('\n\t\n\t') +
-                '\n' +
-                registerFeaturesFoot +
-                '\n'
-            );
-        }
-
-        return readFileSystemFolder().then(convertEntries);
-    }
-
-    return memoize.async(createFeaturesSource, featuresStore);
-})();
-function getFeatures() {
-    return getFeaturesSource().then(function(featuresSource) {
-        return eval(featuresSource); // eslint-disable-line no-eval
-    });
-}
-function getRequiredFeatures(names) {
-    return getFeatures().then(function(features) {
-        var isRequired = function(feature) {
-            return jsenv.Iterable.some(names, function(name) {
-                return feature.match(name);
-            });
-        };
-        var featureHalf = jsenv.Iterable.bisect(features, function(feature) {
-            var required = isRequired(feature);
-            return required;
-        });
-        var featureToEnable = featureHalf[0];
-        var featureToDisable = featureHalf[1];
-
-        featureToDisable.forEach(function(feature) {
-            feature.disable();
-        });
-        featureToEnable.forEach(function(feature) {
-            feature.enable();
-        });
-
-        var enabledFeatures = features.filter(function(feature) {
-            return feature.isEnabled();
-        });
-        return enabledFeatures;
-    });
-}
 function getFeaturesTests(options) {
     var featureNames = options.features;
     var agent = options.agent || 'Firefox/50.0';
@@ -205,10 +70,163 @@ function getFeaturesTests(options) {
             // l'idée c'est donc d'avoir une fonction genre buildFeatureTests
             // idéalement lorsque j'eval ça je devrait récup une liste des features
             // que je n'ai plus qu'à tester en utilisant l'api de jsenv
-            // en gros je pense qu'il "suffit"
+            // sauf que pour ça j'ai besoin du code dans array/feature.js
+            // ici je n'y ai plus accès mais je peux retrouve où le fichier est censé être
+            // lire son contenu, et générer à nouveau du js capable d'enregistrer une liste de features
+            // nan mais c'est bon maintenant je n'aurais qu'à faire
+            // createFeatureSourcesFromFolder(missingTestResultFeatures) en gros
             console.log(missingTestResultFeatures.length, 'test to run');
         });
     });
+}
+function getRequiredFeatures(names) {
+    return getFeatures().then(function(features) {
+        var isRequired = function(feature) {
+            return jsenv.Iterable.some(names, function(name) {
+                return feature.match(name);
+            });
+        };
+        var featureHalf = jsenv.Iterable.bisect(features, function(feature) {
+            var required = isRequired(feature);
+            return required;
+        });
+        var featureToEnable = featureHalf[0];
+        var featureToDisable = featureHalf[1];
+
+        featureToDisable.forEach(function(feature) {
+            feature.disable();
+        });
+        featureToEnable.forEach(function(feature) {
+            feature.enable();
+        });
+
+        var enabledFeatures = features.filter(function(feature) {
+            return feature.isEnabled();
+        });
+        return enabledFeatures;
+    });
+}
+function getFeatures() {
+    return getFileSystemFeatures();
+}
+function getFileSystemFeatures() {
+    return getFeatureSourcesFromFileSystem().then(function(featuresSource) {
+        return eval(featuresSource); // eslint-disable-line no-eval
+    });
+}
+var featuresStore = store.memoryEntry();
+var getFeatureSourcesFromFileSystem = memoize.async(createFeatureSourcesFromFileSystem, featuresStore);
+function createFeatureSourcesFromFileSystem() {
+    return recursivelyReadFolderFeatureNames(featuresFolderPath).then(function(featureNames) {
+        return createFeatureSourcesFromFolder(featureNames, featuresFolderPath);
+    });
+}
+function recursivelyReadFolderFeatureNames(path) {
+    var featureNames = [];
+
+    function readFolder(path) {
+        return fsAsync('readdir', path);
+    }
+
+    function readFolderFeatureNames(parentFeatureName) {
+        var featureFolderPath;
+        if (parentFeatureName) {
+            featureFolderPath = path + '/' + parentFeatureName;
+        } else {
+            featureFolderPath = path;
+        }
+
+        return readFolder(featureFolderPath).then(function(ressourceNames) {
+            var ressourcePaths = ressourceNames.map(function(name) {
+                return featureFolderPath + '/' + name;
+            });
+            var ressourcesPromise = ressourcePaths.map(function(ressourcePath, index) {
+                return fsAsync('stat', ressourcePath).then(function(stat) {
+                    var ressourceName = ressourceNames[index];
+                    if (stat.isDirectory()) {
+                        if (ressourceName[0].match(/[a-z]/)) {
+                            var featureName;
+                            if (parentFeatureName) {
+                                featureName = parentFeatureName + '/' + ressourceName;
+                            } else {
+                                featureName = ressourceName;
+                            }
+
+                            featureNames.push(featureName);
+                            return readFolderFeatureNames(featureName);
+                        }
+                    }
+                    return Promise.resolve();
+                });
+            });
+            return Promise.all(ressourcesPromise);
+        });
+    }
+
+    return readFolderFeatureNames(null).then(function() {
+        return featureNames;
+    });
+}
+function createFeatureSourcesFromFolder(featureNames, folderPath) {
+    var paths = featureNames.map(function(featureName) {
+        return folderPath + '/' + featureName + '/feature.js';
+    });
+
+    return getSources(paths).then(createSources);
+
+    function getSources(paths) {
+        return Promise.all(paths.map(function(path) {
+            return fsAsync('stat', path).then(
+                function(stat) {
+                    if (stat.isFile()) {
+                        return featureTranspiler.transpileFile(path, {
+                            sourceMaps: false,
+                            filename: false,
+                            transform: function(code) {
+                                return code;
+                            }
+                        });
+                    }
+                },
+                function(e) {
+                    if (e && e.code === 'ENOENT') {
+                        return '';
+                    }
+                    return Promise.reject(e);
+                }
+            );
+        }));
+    }
+    function createSources(sources) {
+        var registerFeaturesHead = 'jsenv.registerFeatures(function(registerFeature, transpile) {';
+        var registerFeaturesBody = sources.map(function(source, index) {
+            return createSource(featureNames[index], sources[index]);
+        });
+        var registerFeaturesFoot = '});';
+
+        return (
+            '\n' +
+            registerFeaturesHead +
+            '\n\t' +
+            registerFeaturesBody.join('\n\t\n\t') +
+            '\n' +
+            registerFeaturesFoot +
+            '\n'
+        );
+    }
+    function createSource(featureName, featureCode) {
+        var featureNameSource = "'" + featureName + "'";
+        var featurePropertiesSource = '';
+        if (featureCode) {
+            featurePropertiesSource += 'function(feature) {\n\t';
+            featurePropertiesSource += featureCode;
+            featurePropertiesSource += '\n}';
+        } else {
+            featurePropertiesSource = 'function() {}';
+        }
+
+        return 'registerFeature(' + featureNameSource + ', ' + featurePropertiesSource + ');';
+    }
 }
 
 getFeaturesTests({
@@ -230,7 +248,6 @@ getFeaturesTests({
 // });
 
 module.exports = {
-    getFeaturesSource: getFeaturesSource,
     getFeatures: getFeatures,
     getRequiredFeatures: getRequiredFeatures,
     getFeaturesTests: getFeaturesTests
