bb06bbb41071160419f14cd85c16fa7d881f2c84
Damien Maillard <dmaillard06@gmail.com>
2017-02-14 14:40:47 +0100

    clarify agent.js intent

--- src/features/agents.js
+++ src/features/agents.js
@@ -2,16 +2,15 @@
 // https://github.com/Financial-Times/polyfill-service/blob/master/lib/UA.js
 // https://github.com/3rd-Eden/useragent
 
-var agentMapping = [
+var normalizers = [
     {
-        "name": "blackberry",
-        "variants": [
+        match: [
             "blackberry webkit"
-        ]
+        ],
+        normalize: "blackberry"
     },
     {
-        "name": "firefox",
-        "variants": [
+        match: [
             "pale moon (firefox variant)",
             "firefox namoroka",
             "firefox shiretoko",
@@ -21,113 +20,148 @@ var agentMapping = [
             "microb",
             "mozilladeveloperpreview",
             "iceweasel"
-        ]
+        ],
+        normalize: "firefox"
     },
     {
-        "name": 'firefox-mobile',
-        "variants": [
+        match: [
             "firefox mobile"
-        ]
+        ],
+        normalize: 'firefox-mobile'
     },
     {
-        "name": "opera",
-        "variants": [
+        match: [
             "opera tablet"
-        ]
+        ],
+        normalize: "opera"
     },
     {
-        "name": "opera-mobile",
-        "variants": [
+        match: [
             "op_mob",
             "op_mini"
-        ]
+        ],
+        normalize: "opera-mobile"
     },
     {
-        "name": "chrome",
-        "variants": [
+        match: [
             "chrome mobile",
             "chrome frame",
-            "chromium",
-            {
-                name: "yandex browser",
-                redirect: function() {
-                    // "14.10": ["chrome", 37],
-                    // "14.8": ["chrome", 36],
-                    // "14.7": ["chrome", 35],
-                    // "14.5": ["chrome", 34],
-                    // "14.4": ["chrome", 33],
-                    // "14.2": ["chrome", 32],
-                    // "13.12": ["chrome", 30],
-                    // "13.10": ["chrome", 28]
-                }
-            }
-        ]
+            "chromium"
+        ],
+        normalize: "chrome"
     },
     {
-        "name": "chrome-mobile-ios",
-        "variants": [
+        meta: {
+            "13.10": 28,
+            "13.12": 30,
+            "14.2": 32,
+            "14.4": 33,
+            "14.5": 34,
+            "14.7": 35,
+            "14.8": 36,
+            "14.10": 37
+        },
+        match: function(ua) {
+            return (
+                ua.name === "yandex browser" &&
+                ua.version in this.meta
+            );
+        },
+        normalize: function(ua) {
+            return {
+                name: 'chrome',
+                major: this.meta[ua.version],
+                minor: 0,
+                patch: 0
+            };
+        }
+    },
+    {
+        match: [
             "chrome mobile ios"
-        ]
+        ],
+        normalize: "chrome-mobile-ios"
     },
     {
-        "name": "ie",
-        "variants": [
+        match: [
             "ie large screen",
             "internet explorer",
             "edge",
-            "edge mobile",
-            {
-                name: "uc browser",
-                redirect: function() {
-                    // "9.9.*": ["ie", 10]
-                }
-            }
-        ]
+            "edge mobile"
+        ],
+        normalize: 'ie'
     },
     {
-        "name": "ie-mobile",
-        "variants": [
+        match: function(ua) {
+            return (
+                ua.name === 'uc browser' &&
+                ua.version.major === 9 &&
+                ua.version.minor === 9
+            );
+        },
+        normalize: function() {
+            return {
+                name: 'ie',
+                major: 10,
+                minor: 0,
+                patch: 0
+            };
+        }
+    },
+    {
+        match: [
             "ie mobile"
-        ]
+        ],
+        normalize: "ie-mobile"
     },
     {
-        "name": "safari",
-        "variants": [
-            {
-                name: "phantomjs",
-                redirect: function() {
-                    // ["safari", 5],
-                }
-            }
-        ]
+        match: [
+            "phantomjs"
+        ],
+        normalize: function() {
+            return {
+                name: 'safari',
+                major: 5,
+                minor: 0,
+                patch: 0
+            };
+        }
     },
     {
-        "name": "safari-ios",
-        "variants": [
+        match: [
             "mobile safari",
             "iphone",
             "iphone simulator",
-            "mobile safari uiwebview",
-            {
-                match: function(ua) {
-                    return ua.family === 'facebook' && ua.os.family === 'iOS';
-                },
-                redirect: function(ua) {
-                    return {name: ua.family, major: ua.os.major, minor: ua.os.minor};
-                }
-            }
-        ]
+            "mobile safari uiwebview"
+        ],
+        normalize: "safari-ios"
     },
     {
-        "name": "samsung-mobile",
-        "variants": [
+        match: function(ua) {
+            return (
+                ua.family === 'facebook' &&
+                ua.os.family === 'iOS'
+            );
+        },
+        normalize: function(ua) {
+            return {
+                name: ua.family,
+                major: ua.os.major,
+                minor: ua.os.minor,
+                patch: 0
+            };
+        }
+    },
+    {
+        match: [
             "samsung internet"
-        ]
+        ],
+        normalize: "samsung-mobile"
     }
 ];
 
 module.exports = {
-    knownAgents: require('./agents.json'),
-    mapping: agentMapping,
-    maxUnknownAgents: 10
+    agents: require('./agents.json'),
+    allowedUnknownAgents: 10,
+    normalizers: normalizers
 };
