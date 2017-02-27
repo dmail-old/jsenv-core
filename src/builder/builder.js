97e4c80fbe4209a0f2585698d0aeb1999adce762
Damien Maillard <dmaillard06@gmail.com>
2017-02-27 16:47:20 +0100

    vastly improve generateSourceImporting

--- src/builder/builder.js
+++ src/builder/builder.js
@@ -10,6 +10,7 @@ require('../jsenv.js');
 var store = require('../store.js');
 var memoize = require('../memoize.js');
 var uneval = require('../uneval.js');
+var Iterable = jsenv.Iterable;
 
 function normalizePath(path) {
     return path.replace(/\\/g, '/');
@@ -19,67 +20,166 @@ var rootFolder = normalizePath(path.resolve(__dirname, '../../'));
 var cacheFolder = rootFolder + '/cache';
 var builderCacheFolder = cacheFolder + '/builder';
 var builderCache = store.fileSystemCache(builderCacheFolder);
-// var cwd = normalizePath(process.cwd());
 
-function generateSourceImporting(importDescriptions) {
-    var i = 0;
-    var j = importDescriptions.length;
-    var importSources = [];
-    var collectorSources = [];
+function generateSourceImporting(objects) {
+    var instructions = [];
+    function createInstruction(name, from) {
+        return {
+            namedImports: [
+                {
+                    name: name,
+                    as: null
+                }
+            ],
+            from: from,
+            findNamedImport: function(name) {
+                return Iterable.find(this.namedImports, function(namedImport) {
+                    return namedImport.name === name;
+                });
+            },
+            addNamedImport: function(name) {
+                var existing = this.findNamedImport(name);
+                if (existing) {
+                    throw new Error(
+                        'duplicate import ' + name + ' from ' + this.from
+                    );
+                }
 
-    while (i < j) {
-        var importDescription = importDescriptions[i];
-        var importInstructions = importDescription.import.split(',').map(function(name) { // eslint-disable-line
-            name = name.trim();
-            return {
-                name: name,
-                as: name + '$' + i
-            };
+                this.namedImports.push({
+                    name: name,
+                    as: null
+                });
+            },
+            toSource: function() {
+                var source = 'import {';
+                source += this.namedImports.map(function(namedImport) {
+                    var namedImportSource = '';
+                    namedImportSource += namedImport.name;
+                    if (namedImport.as) {
+                        namedImportSource += ' as ' + namedImport.as;
+                    }
+                    return namedImportSource;
+                }).join(', ');
+                source += '}';
+                source += ' from ';
+                source += "'" + this.from + "'";
+                source += ';';
+                return source;
+            }
+        };
+    }
+    var propertyInstructionsMap = [];
+    objects.forEach(function(object) {
+        Object.keys(object).forEach(function(propertyName) {
+            var propertyImport = object[propertyName];
+
+            var existingInstruction = Iterable.find(instructions, function(instruction) {
+                return instruction.from === propertyImport.from;
+            });
+            if (existingInstruction) {
+                existingInstruction.addNamedImport(propertyImport.name);
+                propertyInstructionsMap.push({
+                    object: object,
+                    property: propertyName,
+                    instruction: existingInstruction
+                });
+            } else {
+                var instruction = createInstruction(propertyImport.name, propertyImport.from);
+                propertyInstructionsMap.push({
+                    object: object,
+                    property: propertyName,
+                    instruction: instruction
+                });
+                instructions.push(instruction);
+            }
         });
+    });
+    // set a unique variable name for every import
+    // to prevent clash and allow collecting thoose variable later
+    var id = 0;
+    instructions.forEach(function(instruction) {
+        instruction.namedImports.forEach(function(namedImport) {
+            id++;
+            namedImport.as = namedImport.name + '$' + id;
+        });
+    });
 
-        var importSource = '';
-        importSource += 'import {';
-        importSource += importInstructions.map(function(importInstruction) {
-            return importInstruction.name + ' as ' + importInstruction.as;
-        }).join(', ');
-        importSource += '}';
-        importSource += ' from ';
-        importSource += "'" + importDescription.from + "'";
-        importSource += ';';
-        importSources.push(importSource);
+    function getObjectPropertyVariableName(object, property) {
+        var foundData = Iterable.find(propertyInstructionsMap, function(data) {
+            return (
+                data.object === object &&
+                data.property === property
+            );
+        });
+        var instruction = foundData.instruction;
+        var importName = object[property].name;
+        var namedImport = instruction.findNamedImport(importName);
+        if (!namedImport) {
+            throw new Error('cannot find named import ' + importName + ' of property ' + property);
+        }
+        return namedImport.as;
+    }
 
-        var collectorSource = '';
-        collectorSource += 'collect({';
-        collectorSource += importInstructions.map(function(importInstruction) {
-            return '"' + importInstruction.name + '": ' + importInstruction.as;
-        }).join(', ');
-        collectorSource += '});';
-        collectorSources.push(collectorSource);
-        i++;
+    function generateImportSource() {
+        return instructions.map(function(instruction) {
+            return instruction.toSource();
+        }).join('\n');
+    }
+    function generateCollectorSources() {
+        return objects.map(function(object) {
+            var objectSource = '{';
+            objectSource += Object.keys(object).map(function(key) { // eslint-disable-line
+                return '"' + key + '": ' + getObjectPropertyVariableName(object, key);
+            }).join(', ');
+            objectSource += '}';
+            return 'collect(' + objectSource + ');';
+        }).join('\n');
     }
 
     return (
-        importSources.join('\n') +
+        generateImportSource() +
         '\n\n' +
         'var collector = [];\n' +
         'function collect(a) {\n' +
         '   collector.push(a);\n' +
         '}' +
         '\n' +
-        collectorSources.join('\n') +
+        generateCollectorSources() +
         '\n' +
         'export default collector;'
     );
 }
-function pickImports(importDescriptors, options) {
+// console.log(
+//     generateSourceImporting(
+//         [
+//             {
+//                 foo: {
+//                     name: 'default',
+//                     from: './foo.js'
+//                 },
+//                 bar: {
+//                     name: 'default',
+//                     from: './bar.js',
+//                 },
+//                 baz: {
+//                     name: 'filename',
+//                     from: './bar.js'
+//                 }
+//             }
+//         ]
+//     )
+// );
+
+function pickImports(importDescription, options) {
     options = options || {};
     var root = options.root;
     var transpiler = options.transpiler;
     var mainExportName = options.mainExportName || 'default';
 
     return builderCache.match({
-        imports: importDescriptors,
-        root: root
+        imports: importDescription,
+        root: root,
+        main: mainExportName
     }).then(function(cacheBranch) {
         var entry = cacheBranch.entry({
             name: 'build.js',
@@ -102,7 +202,7 @@ function pickImports(importDescriptors, options) {
     // }
 
     function build() {
-        var moduleSource = generateSourceImporting(importDescriptors, root);
+        var moduleSource = generateSourceImporting(importDescription, root);
         // console.log('the source', moduleSource);
         var entryId = cuid() + '.js';
         var entryPath = root + '/' + entryId;
@@ -114,7 +214,7 @@ function pickImports(importDescriptors, options) {
                 if (
                     warning.code === 'EVAL' &&
                     normalizePath(warning.loc.file) === normalizePath(
-                        path.resolve(root, './helper/detect.js')
+                        path.resolve(root, './detect-helpers.js')
                     )
                 ) {
                     return;
@@ -151,15 +251,18 @@ function pickImports(importDescriptors, options) {
                     transform: function(code, id) {
                         var normalizedPath = normalizePath(id);
                         if (transpiler && normalizedPath !== entryPath) {
-                            var result = transpiler.transpile(code, {
-                                filename: normalizedPath,
-                                sourceRoot: root
+                            return Promise.resolve(
+                                transpiler.transpile(code, {
+                                    filename: normalizedPath,
+                                    sourceRoot: root
+                                })
+                            ).then(function(result) {
+                                return {
+                                    code: result.code,
+                                    map: result.map
+                                    // ast: result.ast.program
+                                };
                             });
-                            return {
-                                code: result
-                                // we should return ast & sourcemap
-                                // of the transpiler
-                            };
                         }
                     }
                 }
