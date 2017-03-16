2782f5f639aff36c4f4b8d9c039a618863b1af8b
Damien MAILLARD <d.maillard@wks-193.sophia.corp.dailymotion.com>
2017-03-16 17:08:04 +0100

    add node performand.now polyfill

--- src/features/performance/now/fix.js
+++ src/features/performance/now/fix.js
@@ -1,18 +1,43 @@
 import {fixProperty} from '/fix-helpers.js';
+import polymorphPlatform from 'someehre.js';
 
-let nowOffset;
-if (typeof performance === 'object' && performance.timing && performance.timing.navigationStart) {
-    nowOffset = performance.timing.navigationStart;
-} else {
-    nowOffset = Date.now();
-}
-const now = () => {
-    return Date.now() - nowOffset;
-};
+const nowPolymorph = polymorphPlatform({
+    browser() {
+        const offset = (() => {
+            if (typeof performance === 'object' && performance.timing && performance.timing.navigationStart) {
+                return performance.timing.navigationStart;
+            }
+            return Date.now();
+        })();
+        const now = () => {
+            return Date.now() - offset;
+        };
+        return now;
+    },
+    node() {
+        // https://github.com/braveg1rl/performance-now/blob/master/src/performance-now.coffee
+        const hrtime = process.hrtime;
+        const getNanoSeconds = () => {
+            const hourtime = hrtime();
+            return hourtime[0] * 1e9 + hourtime[1];
+        };
+        const offset = (() => {
+            const moduleLoadTime = getNanoSeconds();
+            const upTime = process.uptime() * 1e9;
+            return moduleLoadTime - upTime;
+        })();
+        const now = () => {
+            const ns = getNanoSeconds();
+            const diff = ns - offset;
+            return diff / 1e6;
+        };
+        return now;
+    }
+});
 
 const fix = {
     type: 'inline',
-    value: fixProperty('performance', now)
+    value: fixProperty('performance', nowPolymorph)
 };
 
 export default fix;
