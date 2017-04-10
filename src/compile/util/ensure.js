584f5c6421eaddd24f217eb8a38542a4c01cb24e
Damien Maillard <dmaillard06@gmail.com>
2017-04-10 22:59:31 +0200

    working on ensure api

--- src/compile/util/ensure.js
+++ src/compile/util/ensure.js
@@ -1,20 +1,50 @@
-const ensureThenable = 1
+const spy = require('./spy.js')
+const ensureThenable = require('./ensure-thenable.js')
 
-const defaultMaxDuration = 100
+const defaultMaxDuration = 1000 * 3
 const Thenable = Promise
-const Output = 1
-const fail = 1
-const pass = 1
-// const spy = 1
 
-const before = (fn, ms) => {
+function Output(properties) {
+	Object.assign(this, properties)
+}
+const createOutput = (properties) => {
+	return new Output(properties)
+}
+const isOutput = (a) => {
+	return a instanceof Output
+}
+const pass = (reason, detail) => {
+	return createOutput({
+		status: 'passed',
+		reason,
+		detail,
+	})
+}
+const fail = (reason, detail) => {
+	return createOutput({
+		status: 'failed',
+		reason,
+		detail,
+	})
+}
+// const cast = (value) => {
+// 	if (value === true) {
+// 		value = pass('returned-true')
+// 	}
+// 	else if (value === false) {
+// 		value = fail('returned-false')
+// 	}
+// 	return value
+// }
+
+const terminationBefore = (fn, ms) => {
 	fn = ensureThenable(fn)
-	return () => {
+	function ensureTerminatedBefore() {
 		let id
 		return Thenable.race([
-			fn,
-			new Thenable(function(resolve) {
-				id = setTimeout(function() {
+			fn.apply(this, arguments),
+			new Thenable((resolve) => {
+				id = setTimeout(() => {
 					resolve(fail('still pending after', ms))
 				}, ms)
 			})
@@ -29,8 +59,9 @@ const before = (fn, ms) => {
 			}
 		)
 	}
+	return ensureTerminatedBefore
 }
-const reject = (fn) => {
+const rejection = (fn) => {
 	fn = ensureThenable(fn)
 	return () => {
 		return fn().then(
@@ -41,17 +72,23 @@ const reject = (fn) => {
 		)
 	}
 }
-const at = (fn) => {
-	return (value, transmit) => transmit(fn(value))
+const equals = (expectedValue) => {
+	return (value) => {
+		if (value === expectedValue) {
+			return pass('equals')
+		}
+		return fail('value does not equal', expectedValue)
+	}
 }
-const order = (...spies) => {
+
+const sequencing = (...spies) => {
 	return () => {
 		const calls = []
 		return Promise.all(spies.map((spy, index) => {
-			return spy.then((call) => {
+			return spy.firstCall().then((call) => {
 				if (index > 0) {
 					const previous = calls[index - 1]
-					if (call.id > previous.id) {
+					if (!previous || call.temporalValue > previous.temporalValue) {
 						return fail(`${spy} resolved before ${spies[index - 1]}`)
 					}
 				}
@@ -61,64 +98,65 @@ const order = (...spies) => {
 		}))
 	}
 }
-const called = () => {
-	return (spy) => {
-		if (spy.calls.length < 0) {
-			return fail('not called')
-		}
-		return pass('called')
+const execution = () => {
+	return (spy, transmit) => {
+		return spy.firstCall().then((call) => {
+			if (call) {
+				transmit(call)
+				return pass('executed')
+			}
+		})
 	}
 }
-
-const ensure = (namedTests, {
+const ensure = (produce, namedTests, {
 	maxDuration = defaultMaxDuration,
 } = {}) => {
-	const tests = Object.keys(namedTests).map((testName) => {
+	produce = ensureThenable(produce)
+	const tests = Object.keys(namedTests).map((name) => {
 		return {
-			name: testName,
-			test: namedTests[testName]
+			name,
+			test: namedTests[name]
 		}
 	})
 
-	const run = () => {
+	let run = (...args) => {
 		const compositeDetail = {}
 		let i = 0
 		const j = tests.length
-		let transmittedValue
 		let currentTest
+		let transmittedValue
 
 		const transmit = (value) => {
 			transmittedValue = value
 		}
-		function next() {
+		const runTest = () => {
+			const returnValue = currentTest.test(transmittedValue, transmit)
+			return Thenable.resolve(returnValue).then(
+				(value) => {
+					if (value === true) {
+						return pass('returned-true')
+					}
+					if (value === false) {
+						return fail('returned-false')
+					}
+					if (isOutput(value)) {
+						return value
+					}
+					return pass('resolved', value)
+				},
+				(reason) => {
+					return fail('rejected', reason)
+				}
+			)
+		}
+		const next = () => {
 			if (i === j) {
 				return pass('all-passed')
 			}
 			currentTest = tests[i]
 			i++
 
-			const runTest = () => {
-				const returnValue = currentTest.test(transmittedValue, transmit)
-				return Thenable.resolve(returnValue).then(
-					(value) => {
-						if (value === true) {
-							return pass('returned-true')
-						}
-						if (value === false) {
-							return fail('returned-false')
-						}
-						if (Output.is(value)) {
-							return value
-						}
-						return pass('resolved', value)
-					},
-					(reason) => {
-						return fail('rejected', reason)
-					}
-				)
-			}
-
-			return before(runTest, maxDuration)().then((output) => {
+			return runTest().then((output) => {
 				compositeDetail[currentTest.name] = output
 				if (output.status === 'failed') {
 					return fail(`expectation failed: ${currentTest.name}`, compositeDetail)
@@ -126,17 +164,134 @@ const ensure = (namedTests, {
 				return next()
 			})
 		}
+		const start = () => {
+			return produce(...args).then((value) => {
+				transmit(value)
+				return next()
+			})
+		}
 
-		return next()
+		return start()
 	}
+	run = terminationBefore(run, maxDuration)
 
 	return run
 }
 
 module.exports = ensure
-ensure.at = at
-ensure.reject = reject
-ensure.called = called
-ensure.before = before
-// ensure.timeout would be the opposite of before
-ensure.order = order
+ensure.rejection = rejection
+ensure.execution = execution
+ensure.terminationBefore = terminationBefore
+// ensure.pendingAfter would be the opposite of terminationBefore
+ensure.sequencing = sequencing
+
+{
+	/*
+	voir si y'aurais pas une meilleur api
+
+	je sais pas ptet
+
+const usedHas10AndIsNamedDamien = expect(
+	{age: 10, name: 'damien'},
+	expect(
+		(value) => value.age,
+		equals(10)
+	),
+	expect(
+		(value) => value.name,
+		equals('damien'),
+		isString()
+	)
+)
+
+const spyIsCalledWith10AsFirstArg = expect(
+	spy(),
+	expect(
+		(value) => spy.firstCall()
+		expect(
+			(call) => call.args[0],
+			equals(10)
+		)
+	)
+)
+
+const spySequencing = expect(
+	() => [spy(), spy(), spy()],
+	(spies) => {
+			return sequencing(spies)(
+				...Promise.all(spies.map((spy) => spy.firstCall())
+			)
+		}
+	)
+)
+
+elle est cool cet api, l'avantage c'est que c'est très lisible
+
+*/
+
+	const test = ensure(
+		() => {
+			const method = spy()
+			method('foo')
+			return {
+				value: Promise.resolve(10),
+				method,
+			}
+		},
+		{
+			'value property thenable resolution': ensure(
+				(v) => v.value,
+				{
+					'is 10': equals(10)
+				}
+			),
+			'method': ensure(
+				(v) => v.method,
+				{
+					'called': execution(),
+					'args': (call) => {
+						if (call.args[0] !== 'foo') {
+							return fail('first arg is not foo')
+						}
+						return pass('first arg is foo')
+					}
+				}
+			)
+		}
+	)
+	test().then(
+		(output) => {
+			console.log('test output', output)
+		},
+		(reason) => {
+			console.log('unexpected test error', reason)
+		}
+	)
+}
+{
+	const test = ensure(
+		() => {
+			return [
+				spy(),
+				spy(),
+				spy()
+			]
+		},
+		{
+			'call them': (spies) => {
+				spies.forEach((spy) => spy())
+			},
+			'sequencing': (spies) => {
+				return sequencing(...spies)()
+			}
+		}
+	)
+	test().then(
+		(output) => {
+			console.log('test output', output)
+		},
+		(reason) => {
+			console.log('unexpected test error', reason)
+		}
+	)
+}
--- src/compile/util/spy.js
+++ src/compile/util/spy.js
@@ -1,24 +1,40 @@
+function Call() {
+	const self = this
+	const data = {}
+	let resolve
+	const promise = new Promise((res) => {
+		resolve = res
+	})
+	self.then = (fn) => {
+		return promise.then(fn)
+	}
+	self.populate = (...args) => {
+		data.thisValue = this
+		data.args = args
+		data.temporalValue = Date.now()
+		resolve(data)
+	}
+
+	return self
+}
+const createCall = () => {
+	return new Call()
+}
+
 const spy = (fn) => {
 	const state = {}
 	const calls = []
+	let callCount = 0
 	state.calls = calls
-
-	const call = (thisValue, args) => {
-		const theCall = {
-			thisValue,
-			args,
-		}
-		return theCall
-	}
 
 	const theSpy = function(...args) {
 		const thisValue = this
-		const theCall = call(thisValue, args)
-		state.calls.push(theCall)
+		const call = theSpy.call(callCount)
+		callCount++
+		let value
+		let state
 
 		if (fn && typeof fn === 'function') {
-			let value
-			let state
 			try {
 				value = fn.apply(thisValue, args)
 				state = 'returned'
@@ -27,17 +43,28 @@ const spy = (fn) => {
 				value = e
 				state = 'throwed'
 			}
-			theCall.state = state
-			theCall.value = value
+			call.state = state
+			call.value = value
+		}
 
-			if (state === 'returned') {
-				return value
-			}
+		call.populate.apply(this, args)
+		if (state === 'returned') {
+			return value
+		}
+		if (state === 'throwed') {
 			throw value
 		}
 	}
 	theSpy.state = state
-	theSpy.firstCall = () => state.calls[0]
+	theSpy.call = (index) => {
+		if (index in calls) {
+			return calls[index]
+		}
+		const call = createCall()
+		calls[index] = call
+		return call
+	}
+	theSpy.firstCall = () => theSpy.call(0)
 
 	return theSpy
 }
