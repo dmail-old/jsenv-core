1b2adeb929677c8426f54d5e99e335b214ea195f
Damien Maillard <dmaillard06@gmail.com>
2017-04-12 23:27:48 +0200

    add more api examples (introduce test.pipe)

---
+++ packages/ensure/test/function-parameters.js
@@ -0,0 +1,38 @@
+module.exports = (test) => {
+	const sameValues = 1
+	const transpile = 1
+
+	const suite = test(
+		'function parameters',
+		test(
+			'default',
+			test(
+				'base',
+				test.pipe(
+					() => transpile`(function(defaultA, defaultB) {
+						return function(a = defaultA, b = defaultB) {
+							return [a, b]
+						}
+					})`,
+					'use default with a missing argument',
+					(fn) => {
+						const defaultA = 1
+						const defaultB = 2
+						const a = 3
+						const result = fn(defaultA, defaultB)(a)
+						return sameValues(result, [a, defaultB])
+					},
+					'use default with an explicit undefined argument',
+					(fn) => {
+						const defaultA = 1
+						const defaultB = 2
+						const b = 4
+						const result = fn(defaultA, defaultB)(undefined, b)
+						return sameValues(result, [defaultA, b])
+					}
+				)
+			)
+		)
+	)
+	return suite
+}
---
+++ packages/ensure/test/spy.js
@@ -0,0 +1,28 @@
+module.exports = (test) => {
+	const spy = 1
+	const calledWith = 1
+	const equals = 1
+
+	const suite = test(
+		'exclude is called and exclude the corresponding ressource',
+		test.pipe(
+			() => System.import('parse').then((parse) => {
+				return Promise.all([
+					parse(`./main.js`, __dirname),
+					System.import('transform')
+				]).then(([
+					tree,
+					transform
+				]) => {
+					const exclude = spy(() => true)
+					return transform(tree, {exclude}).then(() => ({tree, exclude}))
+				})
+			}),
+			'exclude called with importee/importer',
+			({exclude}) => calledWith(exclude, 'file.js', 'main.js'),
+			'first ressource is marked as excluded',
+			({tree}) => equals(tree.root.ressources[0].excluded, true)
+		)
+	)
+	return suite
+}
